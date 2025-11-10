568b61b6f606b7d40768926229684e48e64c2f96
JaidenMcD <jmcd0014@student.monash.edu>
2025-11-10 21:51:06 +1100

    Update ptv_api.py

--- ptv_api.py
+++ ptv_api.py
@@ -11,9 +11,9 @@ key = os.getenv("API_KEY")
 
 def getUrl(request):
     request = request + ('&' if ('?' in request) else '?')
-    raw = request+'devid={0}'.format(devId)
-    hashed = hmac.new(key, raw, sha1)
+    raw = f"{request}devid={devId}"
+    hashed = hmac.new(key.encode('utf-8'), raw.encode('utf-8'), sha1)
     signature = hashed.hexdigest()
-    return 'http://tst.timetableapi.ptv.vic.gov.au'+raw+'&signature={1}'.format(devId, signature)
+    return f"http://tst.timetableapi.ptv.vic.gov.au{raw}&signature={signature}"
 
 print(getUrl('/v2/healthcheck'))
