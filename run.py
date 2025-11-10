7ca9f7046736ff7794e457d5694882e8e494e19c
JaidenMcD <jmcd0014@student.monash.edu>
2025-11-10 17:23:07 +1100

    Update run.py

--- run.py
+++ run.py
@@ -8,13 +8,19 @@ running = True
 screen.fill(BACKGROUND_COLOR)
 clock = pygame.time.Clock()
 
+pygame.font.init()
+font = pygame.font.SysFont('Arial', 30) # Font name and size
+
+
 while running:
     pygame.display.update()
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill(BACKGROUND_COLOR)
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, 100, 100), 0)
-    # RENDER YOUR GAME HERE
+    pygame.draw.rect(screen, TEXT_COLOR, pygame.Rect(0, 0, 100, 100), 0)
+    
+    font.render('Hello Pygame!', True, TEXT_COLOR) # Text, antialias, color (white)
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
