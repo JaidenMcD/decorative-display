603d5ea23d5d49133db396af58dda08499316977
JaidenMcD <jmcd0014@student.monash.edu>
2025-11-10 22:31:16 +1100

    Update ptv_api.py

--- ptv_api.py
+++ ptv_api.py
@@ -28,6 +28,7 @@ def get_tram_departures(route_type=1):
     """
     endpoint = f"/v3/departures/route_type/{route_type}/stop/{tram_stop_id}"
     url = getUrl(endpoint)
+    print(url)
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
