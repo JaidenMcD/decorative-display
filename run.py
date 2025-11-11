2785176aa9e4bf43da749934aebebb5f3fdaf987
JaidenMcD <jmcd0014@student.monash.edu>
2025-11-11 16:45:00 +1100

    Update run.py

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ last_update = 0
 font_path = os.path.join(os.path.dirname(__file__), "assets/fonts/PressStart2P-Regular.ttf")
 font_large = pygame.font.Font(font_path, 24)
 font_small = pygame.font.Font(font_path, 16)
+font_xsmall = pygame.font.Font(font_path, 8)
 
 clock = pygame.time.Clock()
 running = True
@@ -37,10 +38,49 @@ while running:
         last_update = current_time
 
     # draw UI
-    dep = tramStop.return_departures()
-    print(dep)
-    screen.blit(font_large.render("NEXT TRAM", True, TEXT_COLOR), (140, 40))
-    screen.blit(font_large.render("NEXT TRAIN", True, TEXT_COLOR), (180, 120))
+    departures = tramStop.return_departures()
+    screen.blit(font_large.render("NEXT TRAM", True, TEXT_COLOR), (133, 20))
+
+    # City Tram Departures
+    city_dep = [d for d in departures if d["direction"] == "city"]
+    outbound_deps = [d for d in departures if d["direction"] != "city"]
+    if city_dep:
+        city = city_dep[0]
+        times = city["countdowns"]
+        if times:
+            mins, secs = times[0].split(':')
+            display_text = "now" if times[0] == "00:00" else f"{int(mins)} m {int(secs)} s"
+            screen.blit(font_small.render(display_text, True, TEXT_COLOR), (307, 81))
+            
+        if len(times) > 1:
+            ftime = times[1].split(':')
+            mins = ftime[0]
+            secs = ftime[1]
+            screen.blit(font_xsmall.render(f'{mins} m {secs} s', True, TEXT_COLOR), (369,106))
+    else:
+        screen.blit(font_small.render("No city trams", True, TEXT_COLOR), (307,81))
+    
+
+
+    # --- Draw Outbound Directions (Bottom Section) ---
+    start_y = 81  # starting Y coordinate
+    spacing = 40  # vertical gap between each direction row
+
+    for i, dep in enumerate(outbound_deps):
+        y = start_y + i * spacing
+        direction_text = f"{dep['route_number']}"
+        screen.blit(font_small.render(direction_text, True, TEXT_COLOR), (10, y))
+
+        # Display up to two countdowns aligned to the right
+        times = dep["countdowns"]
+        if len(times) > 0:
+            mins, secs = times[0].split(':')
+            display_text = "now" if times[0] == "00:00" else f"{int(mins)} m {int(secs)} s"
+            screen.blit(font_small.render(display_text, True, TEXT_COLOR), (80, y))
+        if len(times) > 1:
+            mins, secs = times[1].split(':')
+            display_text = "now" if times[0] == "00:00" else f"{int(mins)} m {int(secs)} s"
+            screen.blit(font_xsmall.render(times[1], True, TEXT_COLOR), (125, y + 20))
 
     
     pygame.display.flip()
