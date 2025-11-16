5b6c42c0231b46c500d72ca441b1e863d181a3cb
JaidenMcD <jmcd0014@student.monash.edu>
2025-11-16 16:49:19 +1100

    Update run.py

--- run.py
+++ run.py
@@ -14,12 +14,13 @@ print(f"device = {device}")
 
 if device == 1:
     print('setting enviroment variables')
+    os.environ["DISPLAY"] = ":0"
     os.environ["SDL_FBDEV"] = "/dev/fb1"
     os.environ["SDL_VIDEODRIVER"] = "fbcon"
     os.environ["SDL_NOMOUSE"] = "1" 
 
 pygame.init()
-#pygame.mouse.set_visible(False)
+pygame.mouse.set_visible(False)
 
 if device == 1:
     screen = pygame.display.set_mode(SCREEN_RES, pygame.FULLSCREEN | pygame.NOFRAME)
